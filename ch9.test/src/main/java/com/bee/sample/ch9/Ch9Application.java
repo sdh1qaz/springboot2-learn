f6876bc18d409dd105661b018be8f971b9651977
SuDenghui <SuDenghui@DESKTOP-6SUDGBB>
2019-05-05 01:28:37 +0800

    第二次提交

--- ch9.test/src/main/java/com/bee/sample/ch9/Ch9Application.java
+++ ch9.test/src/main/java/com/bee/sample/ch9/Ch9Application.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Ch9Application {
+public class Ch9Application { 
 
 	public static void main(String[] args) {
 		SpringApplication.run(Ch9Application.class, args);
