b4cd696c631bc0adfff8d7642b767e68443c98fe
SuDenghui <SuDenghui@DESKTOP-6SUDGBB>
2019-05-05 01:34:46 +0800

    第4次提交

--- ch1.helloword/src/main/java/com/bee/sample/ch1/Ch1Application.java
+++ ch1.helloword/src/main/java/com/bee/sample/ch1/Ch1Application.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Ch1Application {
+public class Ch1Application { 
 
 	public static void main(String[] args) {
 		SpringApplication.run(Ch1Application.class, args);
