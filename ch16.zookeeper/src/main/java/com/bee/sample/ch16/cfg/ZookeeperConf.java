d1ab15cd8b7d989c92d048b5fc06f18eddfe5e59
苏登辉 <dhsu2@iflytek.com>
2019-05-17 22:26:23 +0800

    学习第16章zookeeper

--- ch16.zookeeper/src/main/java/com/bee/sample/ch16/cfg/ZookeeperConf.java
+++ ch16.zookeeper/src/main/java/com/bee/sample/ch16/cfg/ZookeeperConf.java
@@ -34,9 +34,16 @@ public class ZookeeperConf {
 
 	private boolean isLeader = false;
 
+	// 返回一个ZK客户端对象，并注册为spring的bean
 	@Bean
 	public CuratorFramework getCuratorFramework() throws Exception {
+		// CuratorFramework是一个线程安全的类，可以用它完成所有的zk功能
+		// RetryPolicy用于重连策略，当某种原因导致zk不可用的时候进行重连尝试的策略
+		// ExponentialBackoffRetry是一种重连策略，每次重连的间隔会越来越长，1000毛秒是初始化的间隔时间，3代表重连次数
 		RetryPolicy retryPolicy = new ExponentialBackoffRetry(1000, 3);
+
+		// CuratorFramework通过zkUrl和retryPolicy构造
+		// zkUrl是ZooKeeper的连接地址，在application.properties中
 		CuratorFramework client = CuratorFrameworkFactory.newClient(zkUrl, retryPolicy);
 		client.getCuratorListenable().addListener(new CuratorListener() {
 			public void eventReceived(CuratorFramework client, CuratorEvent event) throws Exception {
@@ -44,11 +51,11 @@ public class ZookeeperConf {
 				if (type == CuratorEventType.WATCHED) {
 					WatchedEvent we = event.getWatchedEvent();
 					EventType et = we.getType();
-					if(we.getPath()!=null) {
+					if (we.getPath() != null) {
 						log.info(et + ":" + we.getPath());
 						client.checkExists().watched().forPath(we.getPath());
 					}
-					
+
 				}
 			}
 
@@ -69,12 +76,12 @@ public class ZookeeperConf {
 		// selector.autoRequeue();
 		// selector.start();
 
+		// 调用start开始链接ZooKeeper
 		client.start();
 
 		registerSerivce(client);
 		ServiceInstance<Map> service = findService(client, "book");
 		return client;
-
 	}
 
 	protected void registerSerivce(CuratorFramework client) throws Exception {
@@ -119,7 +126,5 @@ public class ZookeeperConf {
 		}
 
 	}
-
-	
 
 }
